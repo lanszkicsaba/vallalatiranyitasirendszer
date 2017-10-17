8bfb3b23f20aefc2ed0156ea66fbe38a52c18aaf
Morettihun <morettihun@gmail.com>
2017-10-17 23:22:55 +0200

    Asztali Alkalmazás
    Login form készülni kezdett.

---
+++ VIR/VIR/LoginForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace VIR
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPasswd = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPasswd = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(351, 356);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(141, 50);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Belépés";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(351, 219);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(141, 20);
+            this.textBoxUserName.TabIndex = 1;
+            // 
+            // textBoxPasswd
+            // 
+            this.textBoxPasswd.Location = new System.Drawing.Point(351, 289);
+            this.textBoxPasswd.Name = "textBoxPasswd";
+            this.textBoxPasswd.PasswordChar = '*';
+            this.textBoxPasswd.Size = new System.Drawing.Size(141, 20);
+            this.textBoxPasswd.TabIndex = 2;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(378, 192);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(81, 13);
+            this.labelUsername.TabIndex = 3;
+            this.labelUsername.Text = "Felhasználónév";
+            // 
+            // labelPasswd
+            // 
+            this.labelPasswd.AutoSize = true;
+            this.labelPasswd.Location = new System.Drawing.Point(400, 258);
+            this.labelPasswd.Name = "labelPasswd";
+            this.labelPasswd.Size = new System.Drawing.Size(36, 13);
+            this.labelPasswd.TabIndex = 4;
+            this.labelPasswd.Text = "Jelszó";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(348, 424);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 13);
+            this.labelMessage.TabIndex = 5;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 562);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.labelPasswd);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxPasswd);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.buttonLogin);
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.Text = "Bejelentkezés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPasswd;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPasswd;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ VIR/VIR/LoginForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace VIR
+{
+    public partial class LoginForm : Form
+    {
+        private MySqlConnection conn;
+        private string server;
+        private string database;
+        private string dbuid;
+        private string dbpassword;
+        private string connstr;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+          
+            server = "localhost";
+        
+           database = "sys";
+        
+           dbuid = "root";
+            dbpassword = "";
+            connstr = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + dbuid + ";" + "PASSWORD=" + dbpassword + ";"+"Connection Timeout=300;";
+
+            conn = new MySqlConnection(connstr);
+        }
+
+       
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxUserName.Text.ToString() == "" && textBoxPasswd.Text.ToString() == "")
+            {
+                labelMessage.Text = "Kérlek add meg a belépési adatokat.";
+                labelMessage.ForeColor = Color.Red;
+            }
+
+            if (textBoxUserName.Text.ToString()=="")
+            {
+                labelMessage.Text = "A felhasználónév mező üres. Kérlek ellenőrizd.";
+                labelMessage.ForeColor = Color.Red;
+            }
+            if (textBoxPasswd.Text.ToString() == "")
+            {
+                labelMessage.Text = "A jelszó mező üres. Kérlek ellenőrizd.";
+                labelMessage.ForeColor = Color.Red;
+            }
+
+            if (textBoxUserName.Text.ToString() != "" && textBoxPasswd.Text.ToString() != "")
+            {
+                string mquerylogin = "SELECT * FROM sys.asztali where name=\"" + (textBoxUserName.Text.ToLower()) + "\"";               
+                MySqlCommand cmdlogin = new MySqlCommand(mquerylogin, conn);
+                try
+                {
+                    conn.Open();
+
+
+                    string name = "";
+                    string passwd = "";
+                    MySqlDataReader reader = cmdlogin.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        name = reader.GetString("name");
+                        passwd = reader.GetString("passwd");
+                    }
+                    conn.Close();
+                    if ((name != "" && name.ToLower() == textBoxUserName.Text.ToLower()) && (passwd != "" && passwd == textBoxPasswd.Text.ToString()))
+                    {
+                        labelMessage.Text = "Sikeres belépés!";
+                        labelMessage.ForeColor = Color.Green;
+                        var homeForm = new HomeForm();
+                        homeForm.Show();
+                    }
+                    else
+                    {
+                        labelMessage.Text = "Sikertelen bejelentkezés. Kérjük ellenőrizze az adatokat.";
+                        labelMessage.ForeColor = Color.Red;
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    labelMessage.Text = "Hiba lépett fel";
+                    labelMessage.ForeColor = Color.Red;
+                    MessageBox.Show("Kérjük mutassa meg ezt a fejlesztőnek:\n" + ex.Message,"Adatbázis hiba");
+                }
+
+                
+            }
+        }
+    }
+}
