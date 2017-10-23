f99e0b52c56324f505d9ff5ef16f41edbcc5ae26
lanszkicsaba <lanszkics@live.com>
2017-10-23 19:44:03 +0200

    Készlettábla feltöltése

---
+++ VIR/VIR/DBConnect.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace VIRConnect
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "sql11.freemysqlhosting.net";
+            database = "sql11200750";
+            uid = "sql11200750";
+            password = "UEyZHhGBNu";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public MySqlConnection returnConnection()
+        {
+            server = "sql11.freemysqlhosting.net";
+            database = "sql11200750";
+            uid = "sql11200750";
+            password = "UEyZHhGBNu";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            return connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Nincs kapcsolat az adatbázissal. Kérjük vegye fel a kapcsolatot az adminisztrátorral!");
+                        break;
+                }
+                return false;
+            }
+        }
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
--- VIR/VIR/HomeForm.cs
+++ VIR/VIR/HomeForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VIRConnect;
 
 namespace VIR
 {
@@ -15,7 +17,33 @@ namespace VIR
         public HomeForm()
         {
             InitializeComponent();
-            
+
+            DBConnect conn = new DBConnect();
+            MySqlDataAdapter ada = new MySqlDataAdapter("select * from termekek", conn.returnConnection());
+            DataTable dt = new DataTable();
+            ada.Fill(dt);
+     
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+
+                string[] row = {dr["ID"].ToString(),
+                    dr["termeknev"].ToString(),
+                    dr["ar"].ToString(),
+                    dr["mennyiseg"].ToString(),
+                    dr["kategoria"].ToString(),
+                    dr["leiras"].ToString(),
+                    dr["suly"].ToString(),
+                    dr["kep"].ToString(),
+                    dr["keszleten"].ToString()
+                };
+                var listViewItem = new ListViewItem(row);
+
+                listView1.Items.Add(listViewItem);
+
+            }
+
+
         }
 
         private void HomeForm_FormClosing(object sender, FormClosingEventArgs e)
