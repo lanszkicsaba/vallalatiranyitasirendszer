54180968dc0ac092cbf646d25148233c96ddaaa8
lanszkicsaba <lanszkics@live.com>
2017-11-29 15:17:45 +0100

    F.48. Alap login kezelés

--- Honlap/index.php
+++ Honlap/index.php
@@ -5,34 +5,49 @@
         <title></title>
     </head>
     <body>
-        <?php include 'dbconnect.php'; ?>
+        <?php
+        include 'dbconnect.php';
+        session_start();
+        ?>
         <?php
         $conn = MySQLServerConnecter();
         $select = "SELECT kep, termeknev, leiras, keszleten, ar FROM termekek";
         $s = $conn->query($select);
         $conn->close();
-
-        echo "<table>";
-        echo "<tr>"
-        . "<th>Fénykép</th>"
-        . "<th>Terméknév</th>"
-        . "<th>Leírás</th>"
-        . "<th>Készleten</th>"
-        . "<th>Ár</th>"
-        . "</tr>";
-        while ($row = $s->fetch_assoc()) {
-            echo "<tr>"
-            . "<td><img src=./image/" . $row["kep"] . " alt=fenykep style=width:128px;height:128px;></td>"
-            . "<td>" . $row["termeknev"] . "</td>"
-            . "<td>" . $row["leiras"] . "</td>";
-            if ($row["keszleten"] == 0)
-                echo "<td>Nincs készleten</td>";
-            else
-                echo "<td>Van készleten</td>";
-            echo "<td>" . $row["ar"] . " Ft</td>"
-            . "</tr>";
+        
+        if ($_SESSION["login"]!="TRUE" || $_SESSION["login"]!="FALSE") {
+            $_SESSION["login"]="VALAMI";
         }
-        echo "</table>";
+        
+        if ($_SESSION["login"] == "TRUE") {
+            echo "<p><a href=./logout.php>Kijelentkezés</a></p>";
+            
+        } else {
+            echo "<p><a href=./login.php>Bejelentkezés</a></p>";
+            echo "<p><a href=#valami>Regisztráció</a></p>";
+
+            echo "<table>";
+            echo "<tr>"
+            . "<th>Fénykép</th>"
+            . "<th>Terméknév</th>"
+            . "<th>Leírás</th>"
+            . "<th>Készleten</th>"
+            . "<th>Ár</th>"
+            . "</tr>";
+            while ($row = $s->fetch_assoc()) {
+                echo "<tr>"
+                . "<td><img src=./image/" . $row["kep"] . " alt=fenykep style=width:128px;height:128px;></td>"
+                . "<td>" . $row["termeknev"] . "</td>"
+                . "<td>" . $row["leiras"] . "</td>";
+                if ($row["keszleten"] == 0)
+                    echo "<td>Nincs készleten</td>";
+                else
+                    echo "<td>Van készleten</td>";
+                echo "<td>" . $row["ar"] . " Ft</td>"
+                . "</tr>";
+            }
+            echo "</table>";
+        }
         ?>
     </body>
 </html>
