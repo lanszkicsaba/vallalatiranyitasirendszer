9395d5dbd8c5ab76f84d4fd7e52d3293a92e24ca
lanszkicsaba <lanszkics@live.com>
2017-12-15 21:09:04 +0100

    F.58. Loginform design Kicsinosítottam a loginform.cs-t

--- VIR/VIR/LoginForm.Designer.cs
+++ VIR/VIR/LoginForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.buttonLogin = new System.Windows.Forms.Button();
             this.textBoxUserName = new System.Windows.Forms.TextBox();
             this.textBoxPasswd = new System.Windows.Forms.TextBox();
@@ -36,11 +37,16 @@
             this.labelMessage = new System.Windows.Forms.Label();
             this.buttonLogout = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogin
             // 
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonLogin.Image = ((System.Drawing.Image)(resources.GetObject("buttonLogin.Image")));
+            this.buttonLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.buttonLogin.Location = new System.Drawing.Point(255, 357);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(150, 50);
@@ -51,36 +57,42 @@
             // 
             // textBoxUserName
             // 
-            this.textBoxUserName.Location = new System.Drawing.Point(351, 219);
+            this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxUserName.Location = new System.Drawing.Point(307, 223);
             this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(141, 20);
+            this.textBoxUserName.Size = new System.Drawing.Size(237, 26);
             this.textBoxUserName.TabIndex = 1;
+            this.textBoxUserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBoxUserName.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxUserName_KeyUp);
             // 
             // textBoxPasswd
             // 
-            this.textBoxPasswd.Location = new System.Drawing.Point(351, 289);
+            this.textBoxPasswd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPasswd.Location = new System.Drawing.Point(307, 291);
             this.textBoxPasswd.Name = "textBoxPasswd";
             this.textBoxPasswd.PasswordChar = '*';
-            this.textBoxPasswd.Size = new System.Drawing.Size(141, 20);
+            this.textBoxPasswd.Size = new System.Drawing.Size(237, 26);
             this.textBoxPasswd.TabIndex = 2;
+            this.textBoxPasswd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBoxPasswd.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxPasswd_KeyUp);
             // 
             // labelUsername
             // 
             this.labelUsername.AutoSize = true;
-            this.labelUsername.Location = new System.Drawing.Point(378, 192);
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelUsername.Location = new System.Drawing.Point(361, 191);
             this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(81, 13);
+            this.labelUsername.Size = new System.Drawing.Size(121, 17);
             this.labelUsername.TabIndex = 3;
             this.labelUsername.Text = "Felhasználónév";
             // 
             // labelPasswd
             // 
             this.labelPasswd.AutoSize = true;
-            this.labelPasswd.Location = new System.Drawing.Point(400, 258);
+            this.labelPasswd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPasswd.Location = new System.Drawing.Point(391, 261);
             this.labelPasswd.Name = "labelPasswd";
-            this.labelPasswd.Size = new System.Drawing.Size(36, 13);
+            this.labelPasswd.Size = new System.Drawing.Size(54, 17);
             this.labelPasswd.TabIndex = 4;
             this.labelPasswd.Text = "Jelszó";
             // 
@@ -94,6 +106,9 @@
             // 
             // buttonLogout
             // 
+            this.buttonLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonLogout.Image = ((System.Drawing.Image)(resources.GetObject("buttonLogout.Image")));
+            this.buttonLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.buttonLogout.Location = new System.Drawing.Point(425, 357);
             this.buttonLogout.Name = "buttonLogout";
             this.buttonLogout.Size = new System.Drawing.Size(150, 50);
@@ -110,11 +125,22 @@
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(358, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(124, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 562);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.labelMessage);
@@ -127,6 +153,7 @@
             this.Name = "LoginForm";
             this.Text = "Bejelentkezés";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +169,7 @@
         private System.Windows.Forms.Label labelMessage;
         private System.Windows.Forms.Button buttonLogout;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
