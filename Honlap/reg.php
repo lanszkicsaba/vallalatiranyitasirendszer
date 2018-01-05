68d99f3513a7d6a9b89b928c7d5a89ade0d08ff6
lanszkicsaba <lanszkics@live.com>
2018-01-05 21:04:38 +0100

    F.71. Adószámbevitele a regisztrációkor Fontos hogy a felhasználó betudja írjni az adószámát regisztrációkor.

--- Honlap/reg.php
+++ Honlap/reg.php
@@ -2,7 +2,7 @@
 if(isset($_POST['g-recaptcha-response'])&& $_POST['g-recaptcha-response']){
 include 'dbconnect.php';
 $conn = MySQLServerConnecter();
-$query="insert into honlapusers (`Username`, `Password`, `Fullname`, `Address`, `Phonenumer`) values ('".$_POST['f_felhnev']."','".md5($_POST['f_jelszo'])."','".$_POST['f_nev']."','".$_POST['f_cim']."', '".$_POST['f_telefon']."')";
+$query="insert into honlapusers (`Username`, `Password`, `Fullname`, `Address`, `Phonenumer`, `Taxnumber`) values ('".$_POST['f_felhnev']."','".md5($_POST['f_jelszo'])."','".$_POST['f_nev']."','".$_POST['f_cim']."', '".$_POST['f_telefon']."', '".$_POST['f_ado']."')";
 $conn->query($query);
 $conn->close();
 echo "<h1>Sikeres regisztráció!</h1>";
